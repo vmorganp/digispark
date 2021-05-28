0e823d5f6ac693ad00c88f3a2f695170013ec227
vmorganp <31448722+vmorganp@users.noreply.github.com>
2021-05-28 03:06:15 -0500

    this is for the lame people who don't have a temp folder

--- wallpaper_changer/remote_script.ps1
+++ wallpaper_changer/remote_script.ps1
@@ -1,5 +1,5 @@
-Invoke-WebRequest http://www.thecuriosityworkshop.com/wp-content/uploads/2015/03/01-rubberduck-hongkong.jpg -OutFile C:\temp\d.jpg;
+Invoke-WebRequest http://www.thecuriosityworkshop.com/wp-content/uploads/2015/03/01-rubberduck-hongkong.jpg -OutFile $env:UserProfile\d.jpg;
 sleep 2;
-set-itemproperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value 'C:\temp\d.jpg' -Force;
+set-itemproperty -path "HKCU:\Control Panel\Desktop\" -name wallpaper -value "$env:UserProfile\d.jpg" -Force;
 sleep 5;
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;
