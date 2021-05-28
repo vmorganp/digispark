57e418ad2f9b8ccf27ad0ad6fcccd9a7c6dd7de7
vmorganp <31448722+vmorganp@users.noreply.github.com>
2021-05-28 03:12:34 -0500

    for the good folks who have never once started ie

--- wallpaper_changer/remote_script.ps1
+++ wallpaper_changer/remote_script.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest http://www.thecuriosityworkshop.com/wp-content/uploads/2015/03/01-rubberduck-hongkong.jpg -OutFile $env:UserProfile\d.jpg;
+Invoke-WebRequest -UseBasicParsing http://www.thecuriosityworkshop.com/wp-content/uploads/2015/03/01-rubberduck-hongkong.jpg -OutFile $env:UserProfile\d.jpg;
 sleep 2;
 set-itemproperty -path "HKCU:\Control Panel\Desktop\" -name wallpaper -value "$env:UserProfile\d.jpg" -Force;
 sleep 5;
